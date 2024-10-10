c8665f2e52a6b8c04c6939534643137d883cb6a7
Gabrielly Olinsc <gabrielly-04@outlook.com>
2024-10-10 19:21:02 -0300

    feat: implement .env file

--- eslint.config.js
+++ eslint.config.js
@@ -34,5 +34,10 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    env: {
+      "browser": true,
+      "es2021": true,
+      "node": true  // Adicione esta linha
+    },
   },
 ]
